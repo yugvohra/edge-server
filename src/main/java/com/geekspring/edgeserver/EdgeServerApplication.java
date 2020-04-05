b5e6b79f16b380d049a7e616efcebc26f539aea1
Yugesh Vohra <Yugesh.Vohra@mi9.com.au>
2020-04-05 11:44:43 +1000

    [yugvohra]:[Added edgeServer]

---
+++ src/main/java/com/geekspring/edgeserver/EdgeServerApplication.java
@@ -0,0 +1,17 @@
+package com.geekspring.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class EdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekspring/edgeserver/EdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekspring.edgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
